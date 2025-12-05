1851c16711cb50a42b9d3b59e9886742e5696458
yiliasm <yiliasm@gmail.com>
2025-12-05 14:12:47 -0600

    pushing to azure

--- teamtask/db.js
+++ teamtask/db.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const pool = mysql.createPool({
@@ -7,8 +8,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
+  port: 3306,
+  ssl: false,
 });
 
 export default pool;
